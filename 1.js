fb92bf9b0188b5c45056569e1a8fc455c70b546b
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-02-24 10:43:09 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -189,3 +189,18 @@ The middle character(s) of the word represented as a string. */
 function getMiddle(s) {
   return s.slice((s.length - 1) / 2, s.length / 2 + 1);
 }
+
+/**This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z */
+
+function accum(s) {
+  return s
+    .split("")
+    .map((a, i) => a.toUpperCase() + a.toLowerCase().repeat(i))
+    .join("-");
+}
