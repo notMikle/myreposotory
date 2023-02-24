ea97055603b32bd79838cc5471fd4888954f2d7a
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-02-24 10:21:51 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -113,3 +113,18 @@ Hint: Don't forget to check for bad values like null/undefined
 function countSheeps(arrayOfSheeps) {
   return arrayOfSheeps.filter(Boolean).length;
 }
+
+/*Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7 */
+
+function basicOp(o, a, b) {
+  return eval(a + o + b);
+}
