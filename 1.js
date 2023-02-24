714fdf18636d48417eb4dd8ba9b9db986a93ddd4
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-02-24 10:45:44 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -204,3 +204,16 @@ function accum(s) {
     .map((a, i) => a.toUpperCase() + a.toLowerCase().repeat(i))
     .join("-");
 }
+
+/**Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+Examples
+"This is an example!" ==> "sihT si na !elpmaxe"
+"double  spaces"      ==> "elbuod  secaps" */
+
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map((a) => a.split("").reverse().join(""))
+    .join(" ");
+}
