297ae7a9d96d54026475dc95d04a42f7374af032
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-02-24 10:51:17 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -217,3 +217,17 @@ function reverseWords(str) {
     .map((a) => a.split("").reverse().join(""))
     .join(" ");
 }
+
+/**An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false */
+
+function isIsogram(str) {
+  return !str.match(/([a-z]).*\1/i);
+}
