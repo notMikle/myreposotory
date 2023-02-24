d7f967cd2660b0115a770603ea8d91e97ea97d26
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-02-24 10:34:50 +0300

    Update 1.js

--- 1.js
+++ 1.js
@@ -154,3 +154,15 @@ function filter_list(l) {
     return typeof a == "number";
   });
 }
+
+/**Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+
+ */
+function findShort(s) {
+  return Math.min.apply(
+    null,
+    s.split(" ").map((a) => a.length)
+  );
+}
