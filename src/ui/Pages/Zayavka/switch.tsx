03b2b03e25aa99329f31c35715df50f3c991cd9f
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2023-03-05 17:45:40 +0300

    Update switch.tsx

--- src/ui/Pages/Zayavka/switch.tsx
+++ src/ui/Pages/Zayavka/switch.tsx
@@ -2,17 +2,23 @@ import * as React from "react";
 import Switch from "@mui/material/Switch";
 
 export default function ControlledSwitches() {
-  const [checked, setChecked] = React.useState(true);
+  const [checked, setChecked] = React.useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setChecked(event.target.checked);
+    // (event.target.checked)
   };
+  function func_b(e: any) {
+    e.stopPropagation();
+    e.cancelBubble = true; // для IE
+  }
 
   return (
     <Switch
       checked={checked}
       onChange={handleChange}
       inputProps={{ "aria-label": "controlled" }}
+      onClick={func_b}
     />
   );
 }
